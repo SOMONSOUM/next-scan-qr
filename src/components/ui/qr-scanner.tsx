f1389be5aff242239a6ca6743da926ea9dc46533
SOUM SOM ON <somon.soum9@gmail.com>
2024-12-26 17:03:47 +0700

    add qr-scanner to ui

--- src/components/ui/qr-scanner.tsx
+++ src/components/ui/qr-scanner.tsx
@@ -17,7 +17,7 @@ import { useRouter } from "next/navigation";
 import { motion } from "framer-motion";
 
 export const QRScanner = () => {
-  const [, setHasCamera] = useState(false);
+  const [hasCamera, setHasCamera] = useState(false);
   const [isScanning, setIsScanning] = useState(false);
   const [flashlightOn, setFlashlightOn] = useState(false);
   const [scanResult, setScanResult] = useState<string | null>(null);
