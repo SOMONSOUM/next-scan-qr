a79aef6495996b44e0594d020e65338503db35db
SOUM SOM ON <somon.soum9@gmail.com>
2024-12-26 14:01:20 +0700

    update scan qr code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { ScanQRButton } from "@/components/custom/scan-qr-button";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <ScanQRButton />
+    </div>
+  );
 }
